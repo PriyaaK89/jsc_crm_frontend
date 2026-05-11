cab8b0ab44512572b89cbb0a8039db261e1b8207
kumawatPriya <priyakumawat9868@gmail.com>
2026-05-11 18:33:02 +0530

    fixes

--- src/components/layout/CreateteamLayout.jsx
+++ src/components/layout/CreateteamLayout.jsx
@@ -20,8 +20,8 @@ const CreateTeamLayout = ()=>{
                    <MobileTopbar />
                  </Box>
                  <Box
-                   ml={{ base: 5, md: "295px" }}
-                   mr={{base:5, md:5}}
+                   ml={{sm: 2,  base: 2, md: "295px" }}
+                   mr={{sm: 2,base:2, md:5}}
                    pt="5rem"
                    pb={6}
                  >
